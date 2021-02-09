aadf87b3230444e7c401445c7d0274886dc63bd9
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-09 20:43:52 +0200

    Add counter

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // React;
 // ReactDOM;
 
-class Heading extends React.Component {
-  render() {
-    const {title, className, children} = this.props;
-    return React.createElement('h1', {title: title, className: className}, 'Im in', ...children);
+// class Heading extends React.Component {
+//   render() {
+//     const {title, className, children} = this.props;
+//     return React.createElement('h1', {title: title, className: className}, 'Im in', ...children);
+//   }
+// }
+
+// const reactElement = React.createElement(Heading, {titleForHeading: 'HACKERMANS', className:'heading'}, 'test');
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter : 0}
   }
+
+  increment = () => this.setState({counter: this.state.counter + 1});
+  decrement = () => this.setState({counter: this.state.counter - 1});
+
+  render() {
+    const {counter} = this.state;
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement('h1',null, counter),
+      React.createElement('button', {onClick: this.decrement}, '-'),
+      React.createElement('button', {onClick: this.increment}, '+'),
+    );
+  }
+
 }
 
-const reactElement = React.createElement(Heading, {titleForHeading: 'HACKERMANS', className:'heading'}, 'test');
+const reactElement = React.createElement(Counter);
 
 ReactDOM.render(reactElement, document.getElementById('root'));
